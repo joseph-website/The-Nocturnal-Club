3b98fe1d3c28e55f10a3ecada067e0abff014f93
Lohosin-Joseph <jl4335@gmail.com>
2026-09-12 22:54:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/The-Nocturnal-Club/',
+    base: '/The Nocturnal Club/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
