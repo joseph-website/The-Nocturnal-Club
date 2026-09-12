ea2819d9cc266709c076fc585c5e2ba110739e0f
Lohosin-Joseph <jl4335@gmail.com>
2026-09-12 23:04:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/The-Nocturnal-Club/', // 👈 只修復了這一行的路徑設定
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
